b60045ca7c500f5c2c027d8c4e2be48306ebdbcf
Pradeep Kumar <95734296+Impradeep2000@users.noreply.github.com>
2023-10-14 00:19:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,61 @@
-//your code here
-const express = require('express');
-const path = require('path');
+let obj;
 
-const app = express();
+function fetchAndUpdateUserData() {
+  let p = fetch("https://randomuser.me/api/");
 
-app.use(express.static(__dirname))
+  p.then((response) => {
+    console.log(response.status);
+    console.log(response.ok);
+    return response.json();
+  }).then((value2) => {
+    console.log(value2);
+    obj = value2;
+    // Access the large picture URL from the fetched data
+    let largePictureUrl = value2.results[0].picture.large;
+    let name = value2.results[0].name.first + " " + value2.results[0].name.last;
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/main.html'));
+    // Set the HTML content with the image URL
+    document.getElementById("img").innerHTML = `
+      <img src="${largePictureUrl}" alt="Random User Image"></img>
+    `;
+
+    document.getElementById("name").innerHTML = `
+      ${name}
+    `;
+
+    document.getElementById("display").innerHTML = ""; // Clear previous content
+
+  });
+}
+
+fetchAndUpdateUserData();
+
+document.querySelector("[data-attr=age]").addEventListener("click", () => {
+
+  if (obj.results[0].registered) {
+    let age = obj.results[0].registered.age;
+    document.getElementById("display").innerHTML = `
+      Age: ${age}
+    `;
+  } else {
+    
+  }
 });
-//your code here
-app.post('/add', (req, res) => {
-  const {a,b} = req.body;
-  res.status(200).send(a+b);
-  // res.sendFile(path.join(__dirname + '/main.html'));
+
+document.querySelector("[data-attr=email]").addEventListener("click", () => {
+  let email = obj.results[0].email;
+  document.getElementById("display").innerHTML = `
+    Email: ${email}
+  `;
 });
-module.exports = app;
+
+document.querySelector("[data-attr=phone]").addEventListener("click", () => {
+  let phone = obj.results[0].phone;
+  document.getElementById("display").innerHTML = `
+    Phone: ${phone}
+  `;
+});
+
+document.getElementById("getUser").addEventListener("click", () => {
+  fetchAndUpdateUserData();
+});
